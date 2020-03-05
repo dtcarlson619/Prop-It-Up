e2a4e77510fcc9d018638944f6a2588d9a935e6b
d_carlson_dev <53411336+dtcarlson619@users.noreply.github.com>
2020-03-05 15:20:44 -0700

    using state to update age with birthday button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import PersonCard from './components/PersonCard';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <PersonCard firstName="John" lastName="Stevenson" age="21" haircolor="Black"></PersonCard>
-      <PersonCard firstName="Jason" lastName="Bourne" age="33" haircolor="Blonde"></PersonCard>
-      <PersonCard firstName="Sarah" lastName="Stevenson" age="24" haircolor="Black"></PersonCard>
-      <PersonCard firstName="Amy" lastName="Burger" age="44" haircolor="Red"></PersonCard>
+      <PersonCard firstName="John" lastName="Stevenson" age={21} hairColor="Black" />
+      <PersonCard firstName="Jason" lastName="Bourne" age={33} hairColor="Blonde" />
+      <PersonCard firstName="Sarah" lastName="Stevenson" age={22} hairColor="Black" />
+      <PersonCard firstName="Amy" lastName="Burger" age={44} hairColor="Red" />
     </div>
   );
 }
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
-const PersonCard = (props) => (
-    <div> 
-        <h2> {props.lastName}, {props.firstName} </h2>
-        <p>Age: {props.age} </p>
-        <p>Hair Color: {props.haircolor} </p>
-    </div>
-);
+class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            ageCount: props.age
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            ageCount: this.state.ageCount + 1
+        })
+    }
+    render() {
+        const {firstName, lastName, hairColor} = this.props;
+        return (
+            <div> 
+                <h2> {lastName}, {firstName} </h2>
+                <p>Age: {this.state.ageCount} </p>
+                <p>Hair Color: {hairColor} </p>
+                <button onClick={this.handleClick}>Birthday Button for {firstName} {lastName}</button>
+            </div>
+        );
+    }
+}
 
-export default PersonCard
+export default PersonCard;
